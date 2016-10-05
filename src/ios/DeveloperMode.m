c215d7b3bc6b9b6cc09767d09cb0a94b34233820
Ryan Willoughby <ryan.willoughby@adobe.com>
2016-10-05 16:36:31 -0700

    added enableHomeScript feature, iOS only now

---
+++ src/ios/DeveloperMode.m
@@ -0,0 +1,24 @@
+#import "DeveloperMode.h"
+@implementation DeveloperMode
+
+- (void)pluginInitialize {
+    [NSURLProtocol registerClass:[NSURLProtocolNoCache class]];
+}
+
+- (void)enableHomeScript:(CDVInvokedUrlCommand*)command {
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:CDVPageDidLoadNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(pageDidLoad:) name:CDVPageDidLoadNotification object:nil];
+}
+
+- (void)disableHomeScript:(CDVInvokedUrlCommand*)command {
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:CDVPageDidLoadNotification object:nil];
+}
+
+- (void) pageDidLoad:(NSNotification *) notification
+{
+    // Michael Brooks' homepage.js (https://github.com/phonegap/connect-phonegap/blob/master/res/middleware/homepage.js)
+    NSString* tapScript = @"javascript: console.log('adding homepage.js'); (function(){var e={},t={touchstart:'touchstart',touchend:'touchend'};if(window.navigator.msPointerEnabled){t={touchstart:'MSPointerDown',touchend:'MSPointerUp'}}document.addEventListener(t.touchstart,function(t){var n=t.touches||[t],r;for(var i=0,s=n.length;i<s;i++){r=n[i];e[r.identifier||r.pointerId]=r}},false);document.addEventListener(t.touchend,function(t){var n=Object.keys(e).length;e={};if(n===3){t.preventDefault();window.history.back(window.history.length)}},false)})(window)";
+    [self.commandDelegate evalJs:tapScript];
+}
+
+@end
---
+++ www/developer-mode.js
@@ -0,0 +1,21 @@
+
+var exec = cordova.require('cordova/exec');
+
+
+
+/*!
+ * Content Sync Plugin.
+ */
+
+module.exports = {
+
+    /**
+     * Enables injection of home (3-finger-tap) javascript code on page load.
+     *
+     */
+
+    enableHomeScript: function() {
+        exec(null, null, 'DeveloperMode', 'enableHomeScript');
+    }
+
+};
