19988d4d1671a2152a2760f071ddb206fa4d5eb7
timkim <timk@adobe.com>
2016-10-07 15:00:37 -0700

    [#2] - have a config object to enable scripts

--- www/developer-mode.js
+++ www/developer-mode.js
@@ -2,47 +2,57 @@
 var exec = cordova.require('cordova/exec');
 
 module.exports = {
-  
+    enabledScripts : {
+        // these should be set via local storage or some other mechanism
+        'autoreload': true,
+        'console': true,
+        'deploy': true,
+        'homepage': true,
+        'push': true,
+        'refresh': true
+    }
 };
 
-/**
- * Enables 3-finger-tap home script
- * Runs on plugin load.
- */
+if(module.exports.enabledScripts.autoreload) {
+    /**
+     * Enables 3-finger-tap home script
+     * Runs on plugin load.
+     */
 
-(function () {
+    (function () {
 
-  console.log('DeveloperMode: enabling 3-finger home script')
+      console.log('DeveloperMode: enabling 3-finger home script')
 
-  var e = {},
-    t = {
-      touchstart: 'touchstart',
-      touchend: 'touchend'
-    };
+      var e = {},
+        t = {
+          touchstart: 'touchstart',
+          touchend: 'touchend'
+        };
 
-  if (window.navigator.msPointerEnabled) {
-    t = {
-      touchstart: 'MSPointerDown',
-      touchend: 'MSPointerUp'
-    }
-  }
+      if (window.navigator.msPointerEnabled) {
+        t = {
+          touchstart: 'MSPointerDown',
+          touchend: 'MSPointerUp'
+        }
+      }
 
-  document.addEventListener(t.touchstart, function(t) {
-    var n = t.touches || [t],
-      r;
-    for (var i = 0, s = n.length; i < s; i++) {
-      r = n[i];
-      e[r.identifier || r.pointerId] = r
-    }
-  }, false);
+      document.addEventListener(t.touchstart, function(t) {
+        var n = t.touches || [t],
+          r;
+        for (var i = 0, s = n.length; i < s; i++) {
+          r = n[i];
+          e[r.identifier || r.pointerId] = r
+        }
+      }, false);
 
-  document.addEventListener(t.touchend, function(t) {
-    var n = Object.keys(e).length;
-    e = {};
-    if (n === 3) {
-      t.preventDefault();
-      window.history.back(window.history.length)
-    }
-  }, false)
+      document.addEventListener(t.touchend, function(t) {
+        var n = Object.keys(e).length;
+        e = {};
+        if (n === 3) {
+          t.preventDefault();
+          window.history.back(window.history.length)
+        }
+      }, false)
 
-})();
+    })();
+}
